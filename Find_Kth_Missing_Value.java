c6cc85b746bf5cf2fe59de25c446cb66fee50bbe
hetuo <the8@dons.usfca.edu>
2018-05-20 11:36:47 -0700

    New Algorithm added

--- Find_Kth_Missing_Value.java
+++ Find_Kth_Missing_Value.java
@@ -1,1 +1,18 @@
-//test
+Class Solution {
+
+  public int findKthMissingValue(int[] A, int[] B, int k){
+      if (A == null || A.length == 0 || B == null || B.length ==0 || A.length < k)
+        return -1;
+      
+      Set<Integer> set = new HashSet<>();
+      for (int i : B)
+        set.add(i);
+      for (int i = 0; i < A.length; i++){
+        if (!set.contains(A[i]))
+          k--;
+        if (k == 0)
+          return A[i]
+      }
+      return -1
+  }
+}
