9b00cfe5b15879be25676df4acc45e9b2e180e61
hetuo <the8@dons.usfca.edu>
2018-07-01 18:23:59 -0700

    Create ShortestSubarray.java
    lc862

---
+++ ShortestSubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int shortestSubarray(int[] A, int K) {
+        int n = A.length;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            sum[i + 1] = sum[i] + A[i];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int res = n + 1;
+        for (int i = 0; i < n + 1; i++){
+            while(dq.size() > 0 && sum[i] - sum[dq.peekFirst()] >= K)
+                res = Math.min(res, i - dq.pollFirst());
+            while (dq.size() > 0 && sum[i] < sum[dq.peekLast()])
+                dq.pollLast();
+            dq.add(i);
+        }
+        return res > n ? -1 : res;
+    }
+}
